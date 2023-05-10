505a0bfb47318d0cbd5d2b1da783aa17bb5ccb6a
Patryk <63200231+parpok@users.noreply.github.com>
2023-05-10 14:56:41 +0200

    Made some basic math problem functions and acknowledged some problems
    The functions include addition, subtraction, mutliply and division

---
+++ Sources/BasicMath/divide.swift
@@ -0,0 +1,14 @@
+func division() {
+
+    print("Enter the first number of the division")
+    var number1: Int? = Int(readLine()!) ?? 0
+
+    print("Enter the second number of the division")
+    var number2: Int? = Int(readLine()!) ?? 0
+
+    
+    let result: Int = number1! / number2!
+    print("Your equation is [\(number1!) : \(number2!)] and " + "the result of this division is \(result)")
+
+    // TODO - Add error for division by 0
+}
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-HelloWorld()
+// TODO - Add some kind of a menu picker or something to choose what to run.
+
+print("Currently if you clone this project and to see the working functions you'll need to refrence them here")
