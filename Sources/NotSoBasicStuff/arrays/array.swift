058c0a87254ed4fadadd5e27ad088b8f9ddc74b0
Patryk <63200231+parpok@users.noreply.github.com>
2023-05-13 14:40:06 +0200

    arrays

---
+++ Sources/NotSoBasicStuff/arrays/array.swift
@@ -0,0 +1,12 @@
+func arrays(){
+    let Numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print(Numbers)
+    
+    let Names : [String] = ["Pat", "John", "Bob"]
+    print(Names)
+
+    var emptyInts: [Int] = [] 
+    emptyInts = [1, 2, 3, 4, 5]
+    print(emptyInts)
+
+}
